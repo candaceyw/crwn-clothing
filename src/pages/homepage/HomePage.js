a876b39a4f6c6f53c3e26fc800d59c8766eee1e3
Candace Wilson <candaceyw@gmail.com>
2021-06-30 21:25:59 -0400

    homepage broken down into smaller components and created the menu items

---
+++ src/pages/homepage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory';
+import './homepage.scss';
+
+const HomePage = (props) =>(
+  
+  <div className="homepage">
+    <Directory />
+  </div>
+  
+
+)
+
+export default HomePage;
